b5a7d39e7c6fadd922ba1703922c88161475e37a
dipakmandal10975 <dipakmandal10975@gmail.com>
2025-07-03 19:36:01 +0545

    Backend template setup by deepak

---
+++ resources/views/backend/layouts/master.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<!-- Mirrored from dreamguys.co.in/demo/doccure/admin/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 30 Nov 2019 04:12:20 GMT -->
+<head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+        <title>@yield('title')</title>
+		
+		<!-- Favicon -->
+        <link rel="shortcut icon" type="image/x-icon" href="{{ asset('backend/img/favicon.png') }}"
+">
+		
+		<!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="{{ asset('backend/css/bootstrap.min.css') }}">
+		
+		<!-- Fontawesome CSS -->
+        <link rel="stylesheet" href="{{asset('backend/css/font-awesome.min.css') }}">
+		
+		<!-- Feathericon CSS -->
+        <link rel="stylesheet" href="{{asset('backend/css/feathericon.min.css') }}">
+		
+		<link rel="stylesheet" href="{{asset('backend/plugins/morris/morris.css') }}">
+		
+		<!-- Main CSS -->
+        <link rel="stylesheet" href="{{asset('backend/css/style.css') }}">
+		
+		<!--[if lt IE 9]>
+			<script src="assets/js/html5shiv.min.js"></script>
+			<script src="assets/js/respond.min.js"></script>
+		<![endif]-->
+    </head>
+    <body>
+	
+		<!-- Main Wrapper -->
+        <div class="main-wrapper">
+		
+			<!-- Header -->
+           @include('backend.includes.topbar')
+			<!-- /Header -->
+			
+			<!-- Sidebar -->
+           @include('backend.includes.sidebar') 
+			<!-- /Sidebar -->
+			
+			<!-- Page Wrapper -->
+            <div class="page-wrapper">
+			
+                <div class="content container-fluid">
+					
+					<!-- Page Header -->
+					@include('backend.includes.header')
+					<!-- /Page Header -->
+
+					@yield('content')
+					
+				</div>			
+			</div>
+			<!-- /Page Wrapper -->
+		
+        </div>
+		<!-- /Main Wrapper -->
+		
+		<!-- jQuery -->
+        <script src="backend/js/jquery-3.2.1.min.js"></script>
+		
+		<!-- Bootstrap Core JS -->
+        <script src="backend/js/popper.min.js"></script>
+        <script src="backend/js/bootstrap.min.js"></script>
+		
+		<!-- Slimscroll JS -->
+        <script src="backend/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+		
+		<script src="backend/plugins/raphael/raphael.min.js"></script>    
+		<script src="backend/plugins/morris/morris.min.js"></script>  
+		<script src="backend/js/chart.morris.js"></script>
+		
+		<!-- Custom JS -->
+		<script  src="backend/js/script.js"></script>
+		
+    </body>
+
+<!-- Mirrored from dreamguys.co.in/demo/doccure/admin/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 30 Nov 2019 04:12:34 GMT -->
+</html>
