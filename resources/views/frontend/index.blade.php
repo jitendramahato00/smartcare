4a0fa2bc0bb0fe20a8da9d5178324860d0a33841
Jitendra Mahato <mr.jitendramahato00@gmail.com>
2025-07-04 10:14:30 +0545

    Conflict fix

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -4,30 +4,6 @@
 <!-- doccure/  30 Nov 2019 04:11:34 GMT -->
 <head>
 
-<<<<<<< HEAD
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-		<title>Doccure</title>
-		
-		<!-- Favicons -->
-		<link type="image/x-icon" href="assets/img/favicon.png" rel="icon">
-		
-		<!-- Bootstrap CSS -->
-		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
-		
-		<!-- Fontawesome CSS -->
-		<link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css">
-		<link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css">
-		
-		<!-- Main CSS -->
-		<link rel="stylesheet" href="assets/css/style.css">
-		
-		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-		[if lt IE 9]>
-			<script src="assets/js/html5shiv.min.js"></script>
-			<script src="assets/js/respond.min.js"></script>
-		<![endif]
-=======
      <!-- Favicons -->
 <link type="image/x-icon" href="{{ asset('frontend/img/favicon.png') }}" rel="icon">
 
@@ -47,7 +23,6 @@
 			<script src="assets/js/html5shiv.min.js"></script>
 			<script src="assets/js/respond.min.js"></script>
 		<![endif]-->
->>>>>>> 712fdf03b01f1716beffe25d52e4ccb1e3a2b1c7
 	
 	</head>
 	<body>
@@ -874,19 +849,6 @@
 	   <!-- /Main Wrapper -->
 	  
 		<!-- jQuery -->
-<<<<<<< HEAD
-		<script src="assets/js/jquery.min.js"></script>
-		
-		<!-- Bootstrap Core JS -->
-		<script src="assets/js/popper.min.js"></script>
-		<script src="assets/js/bootstrap.min.js"></script>
-		
-		<!-- Slick JS -->
-		<script src="assets/js/slick.js"></script>
-		
-		<!-- Custom JS -->
-		<script src="assets/js/script.js"></script>
-=======
 <script src="{{ asset('frontend/js/jquery.min.js') }}"></script>
 
 <!-- Bootstrap Core JS -->
@@ -899,7 +861,6 @@
 <!-- Custom JS -->
 <script src="{{ asset('frontend/js/script.js') }}"></script>
 
->>>>>>> 712fdf03b01f1716beffe25d52e4ccb1e3a2b1c7
 		
 	</body>
 
